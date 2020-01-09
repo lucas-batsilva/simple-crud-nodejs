b096028e5132715624b9348967767bce35d041e8
lucas-batsilva <lucas.bsilva09@outlook.com>
2020-01-08 23:11:27 -0300

    create: initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+
+const server = express();
+
+server.use(express.json());
+
+const users = ["bruno", "marcelo", "Victor"];
+
+server.get("/users", (req, res) => {
+  res.json(users);
+});
+
+server.get("/users/:index", (req, res) => {
+  const { index } = req.params;
+  res.json(users[index]);
+});
+
+server.post("/users", (req, res) => {
+  const { name } = req.body;
+  users.push(name);
+  return res.json(users);
+});
+
+server.put("/users/:index", (req, res) => {
+  const { index } = req.params;
+  const { name } = req.body;
+  users[index] = name;
+  return res.json(users);
+});
+
+server.delete("/users/:index", (req, res) => {
+  const { index } = req.params;
+  users.splice(index, 1);
+  return res.send();
+});
+
+server.listen(3000);
